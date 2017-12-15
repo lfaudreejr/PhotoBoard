14eebb843a670098e37f8d06781d28b306f87ef7
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-14 23:03:24 -0500

    first navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-navbar toggleable="md" type="dark" class="bg-alt-dark">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">PhotoBoard</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text"   placeholder="Search"/>
+          <b-button size="sm" class="my-2 my-sm-0"  type="submit">Search</b-button>
+        </b-nav-form>
+
+      <b-navbar-nav>
+        <b-nav-item href="#">Sign In</b-nav-item>
+      </b-navbar-nav>
+
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+
+</template>
+
+<style>
+
+</style>
