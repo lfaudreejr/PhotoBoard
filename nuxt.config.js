c0d42386809e63047ed4be4d94da3ae319efea3e
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-14 23:20:15 -0500

    sass initial load

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [ '~/assets/css/main.css' ],
+  css: [ '~/assets/css/main.scss', '~/assets/css/main.css' ],
 
   /*
   ** Plugins to load before mounting the App
