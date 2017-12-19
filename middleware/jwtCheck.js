ca066a18a76e7563a5104cd200b77966c61958ba
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-18 19:55:08 -0500

    server jwtcheck middleware

---
+++ middleware/jwtCheck.js
@@ -0,0 +1,14 @@
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+
+module.exports.jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10,
+    jwksUri: "https://lfaudreejr.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'https://photoboard.herokuapp.com/api',
+  issuer: "https://lfaudreejr.auth0.com/",
+  algorithms: ['RS256']
+})
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const jwtCheck = require('../middleware/jwtCheck')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', jwtCheck, function(req, res, next) {
+  res.send('secured resource');
 });
 
 module.exports = router;
