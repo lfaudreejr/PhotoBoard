315446bc485441417287c84a4877c68a0a02605f
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-20 20:36:21 -0500

    mongo added

---
+++ server/services/mongoService.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient
+
+let mongoUri = process.env.MONGO_URI
+let _db
+
+function getConnection() {
+  return new Promise((resolve, reject) => {
+    if (_db) resolve (_db)
+    return MongoClient.connect(mongoUri, (err, db) => {
+      if (err) return reject(err)
+      _db = db
+      return resolve(_db)
+    })
+  })
+}
+
+function create() {}
+
+function readAll() {}
+
+function update() {}
+
+function destroy() {}
+
+function readOne() {}
+
+module.exports = {
+  create,
+  readAll,
+  update,
+  destroy,
+  readOne
+}
